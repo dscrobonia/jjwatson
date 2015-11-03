0c8a58ba94e0a31cf7c0778434911f1c2fd369a2
David Scrobonia <davidscrobonia@gmail>
2015-11-03 13:30:10 -0800

    removed players from bye week in format_prices

--- dailyprices/format_prices.py
+++ dailyprices/format_prices.py
@@ -1,9 +1,10 @@
 import csv
 
 with open('fdprices.csv', 'rb') as inFile:
-	with open('players.csv', 'wb') as outFile:
-		csvReader = csv.reader(inFile, delimiter=';')
-		csvWriter = csv.writer(outFile)
- 
-		for row in csvReader:
-			csvWriter.writerow([' '.join(row[2].split(', ')[::-1]), row[1], row[10], row[6]])
+   with open('players.csv', 'wb') as outFile:
+      csvReader = csv.reader(inFile, delimiter=';')
+      csvWriter = csv.writer(outFile)
+
+      for row in csvReader:
+         if int(row[6]) > 0:
+            csvWriter.writerow([' '.join(row[2].split(', ')[::-1]), row[1], row[10], row[6]])
