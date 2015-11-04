00ef8bde4574bef8675abf41d5f6cd3380d694ef
David Scrobonia <davidscrobonia@gmail>
2015-11-03 16:02:50 -0800

    cleaned up

--- search.py
+++ search.py
@@ -3,16 +3,6 @@ from Queue import PriorityQueue
 
 class Player:
    
-   #def __init__(self, name, position, value, weight):
-   #   self.name = name
-   #   self.position = position
-   #   self.value = value
-   #   self.weight = weight
-   #   if weight != 0:
-   #       self.ratio = value/weight
-   #   else:
-   #       self.ratio = 0
-
    def __init__(self, args):
       self.name = str(args[0])
       self.position = str(args[1])
@@ -43,14 +33,6 @@ class Node:
       
    def make_left(self, players):
       left = self
-      
-#      #filter out completed positions
-#      for position in self.remStructure:
-#         if self.remStructure[position] == 0:
-#            availPlayers = filter(lambda player: player.position != position, players)
-
-#      #sort
-#      availPlayers.sort()
       left.curIndex = left.curIndex + 1
 
       while left.curIndex < len(players) and left.remStructure[players[left.curIndex].position] == 0:
@@ -82,64 +64,6 @@ class Node:
          if self.remStructure[position] > 0:
             return False
       return True
-
-
-class BadNode:
-   INDEX = 0
-   VALUE = 1
-   WEIGHT = 2
-
-   def __init__(self, value, weight, bound, length, bitString):
-      self.value = value
-      self.weight = weight
-      if weight != 0:
-         self.ratio = value/weight
-      else:
-         weight = 0
-      self.bound = bound
-      self.length = length
-      self.bitString = bitString
-
-   def make_left(self, items, capacity, ratios):
-      ret = Node(self.value, 
-                  self.weight,
-                  0,
-                  self.length + 1, 
-                  self.bitString + '0')
-      ret.find_upper_bound(ratios, capacity)
-
-      return ret
-
-
-   def make_right(self, items, capacity, ratios):
-      ret = Node(self.value + int(items[self.length][Node.VALUE]),
-                  self.weight + int(items[self.length][Node.WEIGHT]), 
-                  0,
-                  self.length + 1, 
-                  self.bitString + '1')
-      ret.find_upper_bound(ratios, capacity)
-
-      return ret
-
-   def find_upper_bound(self, items, capacity):
-      tWeight = self.weight
-      v = self.value
-
-      for i in range(0,len(items)):
-         item = items[i]
-
-         if item.index > self.length:
-            if tWeight + item.weight > capacity:
-               self.bound = v + (capacity - tWeight) * item.ratio
-               return
-
-            v = v + item.value
-            tWeight = tWeight = item.weight
-       
-      self.bound = v + (capacity - tWeight) * items[0].ratio
-
-   def __lt__(self, other):
-      return self.bound < other.bound
 
 def greedy(players, structure, capacity, numberReturned):
    weight = 0
@@ -245,58 +169,3 @@ def branch(players, lineupStructure, capacity, numLineups):
   #    print player.name, player.value
 
    return [best.curLineup]
-   
-def badbranch(capacity, size, items, ratios):
-   q = PriorityQueue()
-   temp = Node(0, 0, ratios[0].ratio * capacity, 0, '')
-   maxV = temp
-
-   q.put(temp)
-
-   while(not q.empty()):
-      temp = q.get()
-      print maxV.value
-
-      if (temp.bound > maxV.value and temp.length < size):
-         left = temp.make_left(items, capacity, ratios)
-
-         if (left.bound > maxV.value):
-            q.put(left)
-
-         if (temp.weight + int(items[temp.length][Node.WEIGHT]) <= capacity):
-            right = temp.make_right(items, capacity, ratios)
-            if (right.value > maxV.value):
-               maxV = right
-
-            if (right.bound > maxV.value):
-               q.put(right)
-
-   print 'Using Branch and Bound the best feasible solution found (that David wrote, its probably soso): '
-   
-   print str(maxV.value) + ' ' + str(maxV.weight)
-   for i in range(maxV.bitString):
-      if (maxV.bitString[i] == '1'):
-         print items[i]
-
-if __name__ == '__main__':
-   capacity = 0
-   size = 0
-   gPlayers = []
-   players = []
-   
-   if len(sys.argv) < 2:
-      print "not enought arguments"
-      sys.exit()
-
-   with open(sys.argv[1]) as inFile:
-      size = int(inFile.next())
-
-      for i in range(1,size+1):
-         row = inFile.next()
-         gPlayers.append(Player([word for word in row.split()]))
-         players.append([word for word in row.split()])
-
-      capacity = int(inFile.next())
-
-      #Search.greedy(capacity, size, gPlayerss)
-      branch(capacity, size, players, gPlayers)
